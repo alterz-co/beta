84cf9a9a3caac1a6ba5cfc297fae9ea7507e332a
alterz01 <alterz.ml@gmail.com>
2019-08-23 22:44:28 +0800

    Setup graphql

---
+++ src/graphql/mutations.js
@@ -0,0 +1,783 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const registerUser = `mutation RegisterUser($input: CreateUserInput!) {
+  registerUser(input: $input) {
+    id
+    name
+    gender
+    email
+    phone
+    password
+    tournaments {
+      items {
+        id
+        title
+        startDate
+        endDate
+        startTime
+        endTime
+        deadline
+        venue
+        description
+        url
+        createdAt
+      }
+      nextToken
+    }
+    announcements {
+      items {
+        id
+        title
+        description
+        createdAt
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const updateUser = `mutation UpdateUser($input: UpdateUserInput!) {
+  updateUser(input: $input) {
+    id
+    name
+    gender
+    email
+    phone
+    password
+    tournaments {
+      items {
+        id
+        title
+        startDate
+        endDate
+        startTime
+        endTime
+        deadline
+        venue
+        description
+        url
+        createdAt
+      }
+      nextToken
+    }
+    announcements {
+      items {
+        id
+        title
+        description
+        createdAt
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const createTournament = `mutation CreateTournament($input: CreateTournamentInput!) {
+  createTournament(input: $input) {
+    id
+    title
+    startDate
+    endDate
+    startTime
+    endTime
+    deadline
+    venue
+    description
+    url
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+    schedule {
+      id
+      description
+      tournament {
+        id
+        title
+        startDate
+        endDate
+        startTime
+        endTime
+        deadline
+        venue
+        description
+        url
+        createdAt
+      }
+    }
+    updates {
+      items {
+        id
+        description
+        createdAt
+      }
+      nextToken
+    }
+    results {
+      items {
+        id
+        date
+        matchNo
+        winner
+        score
+        createdAt
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const updateTournament = `mutation UpdateTournament($input: UpdateTournamentInput!) {
+  updateTournament(input: $input) {
+    id
+    title
+    startDate
+    endDate
+    startTime
+    endTime
+    deadline
+    venue
+    description
+    url
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+    schedule {
+      id
+      description
+      tournament {
+        id
+        title
+        startDate
+        endDate
+        startTime
+        endTime
+        deadline
+        venue
+        description
+        url
+        createdAt
+      }
+    }
+    updates {
+      items {
+        id
+        description
+        createdAt
+      }
+      nextToken
+    }
+    results {
+      items {
+        id
+        date
+        matchNo
+        winner
+        score
+        createdAt
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const deleteTournament = `mutation DeleteTournament($input: DeleteTournamentInput!) {
+  deleteTournament(input: $input) {
+    id
+    title
+    startDate
+    endDate
+    startTime
+    endTime
+    deadline
+    venue
+    description
+    url
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+    schedule {
+      id
+      description
+      tournament {
+        id
+        title
+        startDate
+        endDate
+        startTime
+        endTime
+        deadline
+        venue
+        description
+        url
+        createdAt
+      }
+    }
+    updates {
+      items {
+        id
+        description
+        createdAt
+      }
+      nextToken
+    }
+    results {
+      items {
+        id
+        date
+        matchNo
+        winner
+        score
+        createdAt
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const createSchedule = `mutation CreateSchedule($input: CreateScheduleInput!) {
+  createSchedule(input: $input) {
+    id
+    description
+    tournament {
+      id
+      title
+      startDate
+      endDate
+      startTime
+      endTime
+      deadline
+      venue
+      description
+      url
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+      schedule {
+        id
+        description
+      }
+      updates {
+        nextToken
+      }
+      results {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const updateSchedule = `mutation UpdateSchedule($input: UpdateScheduleInput!) {
+  updateSchedule(input: $input) {
+    id
+    description
+    tournament {
+      id
+      title
+      startDate
+      endDate
+      startTime
+      endTime
+      deadline
+      venue
+      description
+      url
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+      schedule {
+        id
+        description
+      }
+      updates {
+        nextToken
+      }
+      results {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const deleteSchedule = `mutation DeleteSchedule($input: DeleteScheduleInput!) {
+  deleteSchedule(input: $input) {
+    id
+    description
+    tournament {
+      id
+      title
+      startDate
+      endDate
+      startTime
+      endTime
+      deadline
+      venue
+      description
+      url
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+      schedule {
+        id
+        description
+      }
+      updates {
+        nextToken
+      }
+      results {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const createUpdate = `mutation CreateUpdate($input: CreateUpdateInput!) {
+  createUpdate(input: $input) {
+    id
+    description
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+    tournament {
+      id
+      title
+      startDate
+      endDate
+      startTime
+      endTime
+      deadline
+      venue
+      description
+      url
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+      schedule {
+        id
+        description
+      }
+      updates {
+        nextToken
+      }
+      results {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const updateUpdate = `mutation UpdateUpdate($input: UpdateUpdateInput!) {
+  updateUpdate(input: $input) {
+    id
+    description
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+    tournament {
+      id
+      title
+      startDate
+      endDate
+      startTime
+      endTime
+      deadline
+      venue
+      description
+      url
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+      schedule {
+        id
+        description
+      }
+      updates {
+        nextToken
+      }
+      results {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const deleteUpdate = `mutation DeleteUpdate($input: DeleteUpdateInput!) {
+  deleteUpdate(input: $input) {
+    id
+    description
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+    tournament {
+      id
+      title
+      startDate
+      endDate
+      startTime
+      endTime
+      deadline
+      venue
+      description
+      url
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+      schedule {
+        id
+        description
+      }
+      updates {
+        nextToken
+      }
+      results {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const createResult = `mutation CreateResult($input: CreateResultInput!) {
+  createResult(input: $input) {
+    id
+    date
+    matchNo
+    winner
+    score
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+    tournament {
+      id
+      title
+      startDate
+      endDate
+      startTime
+      endTime
+      deadline
+      venue
+      description
+      url
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+      schedule {
+        id
+        description
+      }
+      updates {
+        nextToken
+      }
+      results {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const updateResult = `mutation UpdateResult($input: UpdateResultInput!) {
+  updateResult(input: $input) {
+    id
+    date
+    matchNo
+    winner
+    score
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+    tournament {
+      id
+      title
+      startDate
+      endDate
+      startTime
+      endTime
+      deadline
+      venue
+      description
+      url
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+      schedule {
+        id
+        description
+      }
+      updates {
+        nextToken
+      }
+      results {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const deleteResult = `mutation DeleteResult($input: DeleteResultInput!) {
+  deleteResult(input: $input) {
+    id
+    date
+    matchNo
+    winner
+    score
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+    tournament {
+      id
+      title
+      startDate
+      endDate
+      startTime
+      endTime
+      deadline
+      venue
+      description
+      url
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+      schedule {
+        id
+        description
+      }
+      updates {
+        nextToken
+      }
+      results {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const createAnnouncement = `mutation CreateAnnouncement($input: CreateAnnouncementInput!) {
+  createAnnouncement(input: $input) {
+    id
+    title
+    description
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const updateAnnouncement = `mutation UpdateAnnouncement($input: UpdateAnnouncementInput!) {
+  updateAnnouncement(input: $input) {
+    id
+    title
+    description
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const deleteAnnouncement = `mutation DeleteAnnouncement($input: DeleteAnnouncementInput!) {
+  deleteAnnouncement(input: $input) {
+    id
+    title
+    description
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+  }
+}
+`;
---
+++ src/graphql/queries.js
@@ -0,0 +1,442 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const getUser = `query GetUser($id: ID!) {
+  getUser(id: $id) {
+    id
+    name
+    gender
+    email
+    phone
+    password
+    tournaments {
+      items {
+        id
+        title
+        startDate
+        endDate
+        startTime
+        endTime
+        deadline
+        venue
+        description
+        url
+        createdAt
+      }
+      nextToken
+    }
+    announcements {
+      items {
+        id
+        title
+        description
+        createdAt
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const getTournament = `query GetTournament($id: ID!) {
+  getTournament(id: $id) {
+    id
+    title
+    startDate
+    endDate
+    startTime
+    endTime
+    deadline
+    venue
+    description
+    url
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+    schedule {
+      id
+      description
+      tournament {
+        id
+        title
+        startDate
+        endDate
+        startTime
+        endTime
+        deadline
+        venue
+        description
+        url
+        createdAt
+      }
+    }
+    updates {
+      items {
+        id
+        description
+        createdAt
+      }
+      nextToken
+    }
+    results {
+      items {
+        id
+        date
+        matchNo
+        winner
+        score
+        createdAt
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const listTournaments = `query ListTournaments(
+  $filter: ModelTournamentFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listTournaments(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      title
+      startDate
+      endDate
+      startTime
+      endTime
+      deadline
+      venue
+      description
+      url
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+      schedule {
+        id
+        description
+      }
+      updates {
+        nextToken
+      }
+      results {
+        nextToken
+      }
+    }
+    nextToken
+  }
+}
+`;
+export const getSchedule = `query GetSchedule($id: ID!) {
+  getSchedule(id: $id) {
+    id
+    description
+    tournament {
+      id
+      title
+      startDate
+      endDate
+      startTime
+      endTime
+      deadline
+      venue
+      description
+      url
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+      schedule {
+        id
+        description
+      }
+      updates {
+        nextToken
+      }
+      results {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const listSchedules = `query ListSchedules(
+  $filter: ModelScheduleFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listSchedules(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      description
+      tournament {
+        id
+        title
+        startDate
+        endDate
+        startTime
+        endTime
+        deadline
+        venue
+        description
+        url
+        createdAt
+      }
+    }
+    nextToken
+  }
+}
+`;
+export const getUpdate = `query GetUpdate($id: ID!) {
+  getUpdate(id: $id) {
+    id
+    description
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+    tournament {
+      id
+      title
+      startDate
+      endDate
+      startTime
+      endTime
+      deadline
+      venue
+      description
+      url
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+      schedule {
+        id
+        description
+      }
+      updates {
+        nextToken
+      }
+      results {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const listUpdates = `query ListUpdates(
+  $filter: ModelUpdateFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listUpdates(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      description
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+      tournament {
+        id
+        title
+        startDate
+        endDate
+        startTime
+        endTime
+        deadline
+        venue
+        description
+        url
+        createdAt
+      }
+    }
+    nextToken
+  }
+}
+`;
+export const getResult = `query GetResult($id: ID!) {
+  getResult(id: $id) {
+    id
+    date
+    matchNo
+    winner
+    score
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+    tournament {
+      id
+      title
+      startDate
+      endDate
+      startTime
+      endTime
+      deadline
+      venue
+      description
+      url
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+      schedule {
+        id
+        description
+      }
+      updates {
+        nextToken
+      }
+      results {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const listResults = `query ListResults(
+  $filter: ModelResultFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listResults(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      date
+      matchNo
+      winner
+      score
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+      tournament {
+        id
+        title
+        startDate
+        endDate
+        startTime
+        endTime
+        deadline
+        venue
+        description
+        url
+        createdAt
+      }
+    }
+    nextToken
+  }
+}
+`;
+export const getAnnouncement = `query GetAnnouncement($id: ID!) {
+  getAnnouncement(id: $id) {
+    id
+    title
+    description
+    createdAt
+    user {
+      id
+      name
+      gender
+      email
+      phone
+      password
+      tournaments {
+        nextToken
+      }
+      announcements {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const listAnnouncements = `query ListAnnouncements(
+  $filter: ModelAnnouncementFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listAnnouncements(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      title
+      description
+      createdAt
+      user {
+        id
+        name
+        gender
+        email
+        phone
+        password
+      }
+    }
+    nextToken
+  }
+}
+`;
