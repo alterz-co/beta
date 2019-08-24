9b33fc5a0fd7a964a05215b91e0a614ea2d99975
alterz01 <alterz.ml@gmail.com>
2019-08-24 15:04:37 +0800

    Scaffold registration and login form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import Landing from './components/Landing';
+import Register from './components/Register';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path={ROUTES.LANDING} component={Landing} />
+          <Route exact path={ROUTES.REGISTER} component={Register}/>
         </Switch>
       </BrowserRouter>
     );
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import * as ROUTES from '../constants/routes';
+import logo from '../logos/landing.svg';
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-        Landing
-      </div>
+      <section className="section container">
+        <div className="row">
+          <img alt="logo" className="col s12 m12 l12" src={logo} style={{ height: 200 }}/>
+          <div className="col s12 l12">
+            <form style={{ padding: 50 }}>
+              <div className="input-field">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                />
+                <label>Email</label>
+              </div>
+              <div className="input-field">
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                />
+                <label>Password</label>
+              </div>
+              <p>
+                Forget password?{' '}
+                <a href="" className="grey-text">Reset Password</a>
+              </p>
+              <a class="btn-large col s12 m12 l12 black white-text" style={{ marginTop: 20, marginBottom: 20 }}>
+                Sign In
+              </a>
+              <p className="center-align">
+                No account?{' '}
+                <a href={ROUTES.REGISTER} className="grey-text">Create account</a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </section>
     );
   }
 }
