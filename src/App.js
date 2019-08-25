8cb385ef590e6af71b44cb0004d46d336438fb67
alterz01 <alterz.ml@gmail.com>
2019-08-25 14:07:53 +0800

    Add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import Landing from './components/Landing';
+import Terms from './components/Terms';
+import Footer from './components/Footer';
 import Home from './components/Home';
 import Register from './components/Register';
 import TournamentDetails from './components/tournaments/TournamentDetails';
@@ -25,27 +27,33 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route exact path={ROUTES.LANDING} component={Landing} />
-          <Route exact path={ROUTES.HOME} component={Home}/>
-          <Route exact path={ROUTES.REGISTER} component={Register}/>
-          <Route path={ROUTES.TOURNAMENT_UPDATES_ADD} component={TournamentUpdatesAdd}/>
-          <Route path={ROUTES.TOURNAMENT_RESULTS_ADD} component={TournamentResultsAdd}/>
-          <Route path={ROUTES.TOURNAMENT_DETAILS} component={TournamentDetails}/>
-          <Route path={ROUTES.ANNOUNCEMENTS} component={Announcements}/>
-          <Route exact path={ROUTES.PROFILE} component={Profile}/>
-          <Route exact path={ROUTES.LUCKY_DRAW} component={LuckyDraw}/>
-          <Route exact path={ROUTES.ORGANISERS} component={OrgHome}/>
-          <Route exact path={ROUTES.ORGANISERS_PROFILE} component={OrgProfile}/>
-          <Route exact path={ROUTES.ORGANISERS_TOURNAMENT_ADD} component={OrgTournamentAdd}/>
-          <Route path={ROUTES.ORGANISERS_TOURNAMENT_EDIT} component={OrgTournamentEdit}/>
-          <Route path={ROUTES.ORGANISERS_TOURNAMENT_DETAILS} component={OrgTournamentDetails}/>
-          <Route path={ROUTES.ORGANISERS_UPDATES_EDIT} component={OrgTournamentUpdatesEdit}/>
-          <Route path={ROUTES.ORGANISERS_RESULTS_EDIT} component={OrgTournamentResultsEdit}/>
-          <Route path={ROUTES.ORGANISERS_SCHEDULE_EDIT} component={OrgTournamentScheduleEdit}/>
-          <Route exact path={ROUTES.ORGANISERS_ANNOUNCEMENT_ADD} component={OrgAnnouncementsAdd}/>
-          <Route path={ROUTES.ORGANISERS_ANNOUNCEMENT_EDIT} component={OrgAnnouncementsEdit}/>
-        </Switch>
+        <div>
+          <div style={{ marginBottom: 40 }}>
+            <Switch>
+              <Route exact path={ROUTES.LANDING} component={Landing} />
+              <Route exact path={ROUTES.TERMS} component={Terms} />
+              <Route exact path={ROUTES.HOME} component={Home}/>
+              <Route exact path={ROUTES.REGISTER} component={Register}/>
+              <Route path={ROUTES.TOURNAMENT_UPDATES_ADD} component={TournamentUpdatesAdd}/>
+              <Route path={ROUTES.TOURNAMENT_RESULTS_ADD} component={TournamentResultsAdd}/>
+              <Route path={ROUTES.TOURNAMENT_DETAILS} component={TournamentDetails}/>
+              <Route path={ROUTES.ANNOUNCEMENTS} component={Announcements}/>
+              <Route exact path={ROUTES.PROFILE} component={Profile}/>
+              <Route exact path={ROUTES.LUCKY_DRAW} component={LuckyDraw}/>
+              <Route exact path={ROUTES.ORGANISERS} component={OrgHome}/>
+              <Route exact path={ROUTES.ORGANISERS_PROFILE} component={OrgProfile}/>
+              <Route exact path={ROUTES.ORGANISERS_TOURNAMENT_ADD} component={OrgTournamentAdd}/>
+              <Route path={ROUTES.ORGANISERS_TOURNAMENT_EDIT} component={OrgTournamentEdit}/>
+              <Route path={ROUTES.ORGANISERS_TOURNAMENT_DETAILS} component={OrgTournamentDetails}/>
+              <Route path={ROUTES.ORGANISERS_UPDATES_EDIT} component={OrgTournamentUpdatesEdit}/>
+              <Route path={ROUTES.ORGANISERS_RESULTS_EDIT} component={OrgTournamentResultsEdit}/>
+              <Route path={ROUTES.ORGANISERS_SCHEDULE_EDIT} component={OrgTournamentScheduleEdit}/>
+              <Route exact path={ROUTES.ORGANISERS_ANNOUNCEMENT_ADD} component={OrgAnnouncementsAdd}/>
+              <Route path={ROUTES.ORGANISERS_ANNOUNCEMENT_EDIT} component={OrgAnnouncementsEdit}/>
+            </Switch>
+          </div>
+          <Footer/>
+        </div>
       </BrowserRouter>
     );
   }
