ffbd08312f76b182652dd0417159e9168d609b40
Qin Zhengquan <qinzhengquan@gmail.com>
2019-09-09 17:52:20 +0800

    fix: fixup orgprofile

--- src/components/organisers/OrgProfile.js
+++ src/components/organisers/OrgProfile.js
@@ -1,32 +1,39 @@
 import React, { Component } from 'react';
-import { API, graphqlOperation } from 'aws-amplify';
-import { getUser } from '../../graphql/queries';
-import OrgNav from './OrgNav';
+// import { API, graphqlOperation } from 'aws-amplify';
+// import { getUser } from '../../graphql/queries';
+import { logoutUser } from '../../redux/actions/userActions';
+import { connect } from 'react-redux';
 
 class Profile extends Component {
+  handleSignout = () => {
+    this.props.onLogoutUser(this.props.history);
+  };
 
-  state = {
-    user: ''
-  }
-
-  async componentDidMount() {
-    const user = await API.graphql(graphqlOperation(getUser, { id: '08baa991-ee93-4563-9ac1-b6eb657f9527' }));
-    this.setState({ user: user.data.getUser });
-  }
+  // state = {
+  //   user: ''
+  // }
+  //
+  // async componentDidMount() {
+  //   const user = await API.graphql(graphqlOperation(getUser, { id: '08baa991-ee93-4563-9ac1-b6eb657f9527' }));
+  //   this.setState({ user: user.data.getUser });
+  // }
 
   render() {
-    const { user } = this.state;
+    const { user } = this.props;
 
     return (
       <div>
         <div className="container" style={{ marginTop: 100 }}>
           <div className="row">
             <div className="col s12 l12">
-              <h2 className="center-align">{user.name}</h2>
-              <p>Gender: {user.gender}</p>
-              <p>Email: {user.email}</p>
-              <p>Phone: {user.phone}</p>
-              <a className="btn-small col s12 m12 l12 black white-text">
+              <h2 className="center-align">{user ? user.name : ''}</h2>
+              <p>Gender: {user ? user.gender : ''}</p>
+              <p>Email: {user ? user.email : ''}</p>
+              <p>Phone: {user ? user.phone : ''}</p>
+              <a
+                onClick={this.handleSignout}
+                className="btn-small col s12 m12 l12 black white-text"
+              >
                 SIGN OUT
               </a>
             </div>
@@ -37,4 +44,17 @@ class Profile extends Component {
   }
 }
 
-export default Profile;
+const mapStateToProps = state => ({
+  user: state.user.user
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onLogoutUser: history => dispatch(logoutUser(history))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Profile);
